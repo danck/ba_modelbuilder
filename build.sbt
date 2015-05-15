b4229a2bd138a1d544e5da28288d3a060d5d2285
Daniel Kirchner <daniel.c.kirchner@googlemail.com>
2015-05-15 12:37:56 +0200

    model building started

--- build.sbt
+++ build.sbt
@@ -1,24 +1,14 @@
-/*name := "Model Builder"
-
-version := "1.0"
-
-scalaVersion := "2.10.4"
-
-libraryDependencies += "org.apache.spark" %% "spark-core" % "provided"
-libraryDependencies += "org.apache.spark" %% "spark-mllib" % "1.3.0"
-*/
 lazy val root = (project in file(".")).
   settings(
     name := "model-builder",
     version := "1.0",
     scalaVersion := "2.10.4",
-    mainClass in Compile := Some("de.haw.bachelorthesis.dkirchner.ModelBuilder")
+    mainClass in Compile := Some("de.haw.bachelorthesis.dkirchner.ModelBuilder"),
+    libraryDependencies ++= Seq(
+      "org.apache.spark" %% "spark-core" % "1.3.0" % "provided",
+      "org.apache.spark" % "spark-mllib_2.10" % "1.3.0"
+    )
   )
-
-libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "1.3.0" % "provided",
-  "org.apache.spark" %% "spark-mllib" % "1.3.0"
-)
 
 // META-INF discarding
 mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) =>
--- src/main/scala/de/haw/bachelorthesis/dkirchner/ModelBuilder.scala
+++ src/main/scala/de/haw/bachelorthesis/dkirchner/ModelBuilder.scala
@@ -8,7 +8,7 @@ package de.haw.bachelorthesis.dkirchner
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext
-import org.apache.spark.mllib.feature.HashingTF
+import org.apache.spark.mllib.feature.{IDF, HashingTF}
 import org.apache.spark.mllib.linalg.Vector
 
 /**
@@ -16,8 +16,26 @@ import org.apache.spark.mllib.linalg.Vector
  */
 object ModelBuilder {
   def main (args: Array[String]) {
-    val sparkConf = new SparkConf().setAppName("Model Builder").setMaster("local")
+    if (args.length < 2) {
+      System.err.println("Usage: ModelBuilder <master> <textfile>")
+      System.exit(1)
+    }
+
+    val Array(master, textFile) = args.take(2)
+    val sparkConf = new SparkConf().setAppName("Model Builder").setMaster(master)
     val sc = new SparkContext(sparkConf)
+
+    val documents: RDD[Seq[String]] = sc.textFile(textFile).map(_.split(" ").toSeq)
+
+    val hashingTF = new HashingTF()
+    val tf: RDD[Vector] = hashingTF.transform(documents)
+
+    tf.cache()
+    val idf = new IDF().fit(tf)
+    val tfidf: RDD[Vector] = idf.transform(tf)
+
+    tfidf.foreach(elem => println(elem))
+
     println("SUCCESS")
   }
 }
