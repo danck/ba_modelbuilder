118c2ec9faf2de01fa14375b69bce27c9aee3eaa
Daniel Kirchner <daniel.c.kirchner@googlemail.com>
2015-05-24 08:19:30 +0200

    .

---
+++ src/test/scala/de/haw/bachelorthesis/dkirchner/tests/ModelBuilderTests.scala
@@ -0,0 +1,27 @@
+package de.haw.bachelorthesis.dkirchner.tests
+
+import org.scalatest.FunSuite
+import org.apache.spark.mllib.linalg.{SparseVector, Vectors, Vector}
+
+/**
+ * Created by Daniel on 24.05.2015.
+ */
+class ModelBuilderTests extends FunSuite {
+  val vector1 = Vectors.sparse(2,Array(0),Array(0.1)).asInstanceOf[SparseVector]
+  val vector2 = Vectors.sparse(2,Array(1),Array(0.1)).asInstanceOf[SparseVector]
+  val expected1 = Vectors.sparse(2,Array(0, 1),Array(0.1, 0.1)).asInstanceOf[SparseVector]
+
+  val vector3 = Vectors.sparse(2,Array(1),Array(0.1)).asInstanceOf[SparseVector]
+  val vector4 = Vectors.sparse(2,Array(1),Array(0.1)).asInstanceOf[SparseVector]
+  val expected2 = Vectors.sparse(2,Array(1),Array(0.2)).asInstanceOf[SparseVector]
+
+  test("distinct: mergeSparseVectors(vector1, vector2)") {
+    val result1 = de.haw.bachelorthesis.dkirchner.ModelBuilder.mergeSparseVectors(vector1, vector2)
+    assert(result1.equals(expected1))
+  }
+
+  test("non-distinct: mergeSparseVectors(vector3, vector4)") {
+    val result2 = de.haw.bachelorthesis.dkirchner.ModelBuilder.mergeSparseVectors(vector3, vector4)
+    assert(result2.equals(expected2))
+  }
+}
